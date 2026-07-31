3b00a7ccfd5139bbc2d8dc5bb316a807b9705389
William Johnson <152228009+wjohnson31@users.noreply.github.com>
2026-07-30 21:18:57 -0400

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.isalfnum(s[l]):
+                l += 1
+            while l < r and not self.isalfnum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                print(s[l])
+                print(s[r])
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+        
+
+    def isalfnum(self, c) -> bool:
+        return (ord('A') <= ord(c) <= ord('Z') 
+                or ord('a') <= ord(c) <= ord('z')        
+                or ord('0') <= ord(c) <= ord('9'))
